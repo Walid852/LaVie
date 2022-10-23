536b955887de5d9f268d3da3500a73989ad239ed
diaabadr <diaabadr82@gmail.com>
2022-10-23 18:47:14 +0200

    deleting quizzes table

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { FacebookStrategy } from './auth/facebook.strategy';
+import { QuizzesModule } from './quizzes/quizzes.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { FacebookStrategy } from './auth/facebook.strategy';
     AuthModule,
     UsersModule,
     PrismaModule,
+    QuizzesModule,
   ],
   providers: [PrismaService],
 })
